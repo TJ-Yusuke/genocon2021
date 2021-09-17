f615570da01e388539200a8e715b080c4edafc95
TJ-Yusuke <jeeproud@gmail.com>
2021-09-17 23:04:59 +0900

    practice1完成

--- Practice1/main.go
+++ Practice1/main.go
@@ -6,16 +6,39 @@ import (
 	"os"
 )
 
+func attachBase(base string) string {
+	switch base {
+	case "A":
+		return "T"
+	case "T":
+		return "A"
+	case "G":
+		return "C"
+	case "C":
+		return "G"
+	default:
+		return ""
+	}
+}
+
+func crateStrand(strand string) string {
+	var result string
+	for _, base := range strand {
+		result = attachBase(string([]rune{base})) + result
+	}
+	return result
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	var base []string
-	for {
-		if scanner.Scan() {
+	scanner.Split(bufio.ScanWords)
+	var i int
+	for scanner.Scan() {
+		if i > 0 {
 			in := scanner.Text()
-			base = append(base, in)
-		} else {
-			break
+			resultStrand := crateStrand(in)
+			fmt.Println(resultStrand)
 		}
+		i++
 	}
-	fmt.Println(base)
 }
